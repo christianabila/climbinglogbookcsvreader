f09de09ab046f698bcdc73cad50be49011e7b81f
Christian Abila <christian@abila.at>
2022-12-03 18:21:21 +0100

    create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ClimbingLogbook;
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Mustache_Engine;
+use Mustache_Loader_FilesystemLoader;
+use Dotenv;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$appname = $_ENV['APPNAME'];
+$version = $_ENV['VERSION'];
+
+$bootstrap_css = $_ENV['BOOTSTRAP_CSS'];
+$bootstrap_js = $_ENV['BOOTSTRAP_JS'];
+
+$mustacheEngine = new Mustache_Engine([
+    'entity_flags' => ENT_QUOTES,
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/views'),
+]);
+
+echo $mustacheEngine->render('mainframe', [
+    'appname' => $appname,
+    'bootstrap_css' => $bootstrap_css,
+    'bootstrap_js' => $bootstrap_js,
+]);
