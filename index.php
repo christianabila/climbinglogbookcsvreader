0c8ad1a36d1ceea2434d53fa78d5954755c0b6f9
Christian Abila <christian@abila.at>
2022-12-25 18:06:26 +0100

    use new CSVController and DatabaseController

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ namespace ClimbingLogbook;
 
 require __DIR__ . '/vendor/autoload.php';
 
+use ClimbingLogbook\Controller\CSVController;
+use ClimbingLogbook\Controller\DatabaseController;
 use ClimbingLogbook\Controller\UploadController;
 use Mustache_Engine;
 use Mustache_Loader_FilesystemLoader;
 use Dotenv;
+use Exception;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -21,6 +24,16 @@ $bootstrap_js = $_ENV['BOOTSTRAP_JS'];
 if (isset($_FILES['csvfile'])) {
     UploadController::validate($_FILES['csvfile']);
     $localFileURI = UploadController::save($_FILES['csvfile']);
+
+    $fileHandle = fopen($localFileURI, "r");
+
+    if (!$fileHandle) {
+        throw new Exception("Could not open file!");
+    }
+
+    $logentries = CSVController::extract($fileHandle);
+
+    DatabaseController::save($logentries);
 }
 
 $mustacheEngine = new Mustache_Engine([
