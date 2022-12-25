9b15581dcaa1daebb1664912292f1d4012d25181
Christian Abila <christian@abila.at>
2022-12-25 15:38:30 +0100

    use UploadController

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ namespace ClimbingLogbook;
 
 require __DIR__ . '/vendor/autoload.php';
 
+use ClimbingLogbook\Controller\UploadController;
 use Mustache_Engine;
 use Mustache_Loader_FilesystemLoader;
 use Dotenv;
@@ -16,6 +17,11 @@ $version = $_ENV['VERSION'];
 
 $bootstrap_css = $_ENV['BOOTSTRAP_CSS'];
 $bootstrap_js = $_ENV['BOOTSTRAP_JS'];
+
+if (isset($_FILES['csvfile'])) {
+    UploadController::validate($_FILES['csvfile']);
+    $localFileURI = UploadController::save($_FILES['csvfile']);
+}
 
 $mustacheEngine = new Mustache_Engine([
     'entity_flags' => ENT_QUOTES,
