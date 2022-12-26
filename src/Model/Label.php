59f280aafca5e11e2dbf92ffd5b9b64319e0ec59
Christian Abila <christian@abila.at>
2022-12-26 08:11:42 +0100

    build basic methods for Label class

--- src/Model/Label.php
+++ src/Model/Label.php
@@ -2,9 +2,45 @@
 
 namespace ClimbingLogbook\Model;
 
+use ValueError;
+
 class Label
 {
     private int $id;
 
     private string $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->setId($id);
+        $this->setName($name);
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        if ($id < 0) {
+            throw new ValueError("Invalid id value!");
+        }
+
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        if (empty($name)) {
+            throw new ValueError("Invalid name!");
+        }
+
+        $this->name = $name;
+    }
 }
