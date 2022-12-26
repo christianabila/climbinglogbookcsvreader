18fbff3a5cadd6411296534476664f891c5f1145
Christian Abila <christian@abila.at>
2022-12-26 16:14:38 +0100

    add upload result communication and use new Controllers

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ namespace ClimbingLogbook;
 require __DIR__ . '/vendor/autoload.php';
 
 use ClimbingLogbook\Controller\CSVController;
-use ClimbingLogbook\Controller\DatabaseController;
+use ClimbingLogbook\Controller\LogbookController;
 use ClimbingLogbook\Controller\UploadController;
 use Mustache_Engine;
 use Mustache_Loader_FilesystemLoader;
@@ -21,28 +21,44 @@ $version = $_ENV['VERSION'];
 $bootstrap_css = $_ENV['BOOTSTRAP_CSS'];
 $bootstrap_js = $_ENV['BOOTSTRAP_JS'];
 
+$uploadresult = null;
+
+// A file is being uploaded. Process it here.
 if (isset($_FILES['csvfile'])) {
-    UploadController::validate($_FILES['csvfile']);
-    $localFileURI = UploadController::save($_FILES['csvfile']);
+    try {
+        UploadController::validate($_FILES['csvfile']);
+        $localFileURI = UploadController::save($_FILES['csvfile']);
 
-    $fileHandle = fopen($localFileURI, "r");
+        $fileHandle = fopen($localFileURI, "r");
+    
+        if ($fileHandle) {
+            $logentries = CSVController::extract($fileHandle);
 
-    if (!$fileHandle) {
-        throw new Exception("Could not open file!");
+            LogbookController::save($logentries);
+
+            // Say something about the result of the save process.
+            $uploadresult['success'] = "File uploaded and saved successfully!";
+        } else {
+            $uploadresult['error'] = "Could not open file!";
+        }
+    } catch (Exception $e) {
+        $uploadresult['error'] = $e->getMessage();
     }
-
-    $logentries = CSVController::extract($fileHandle);
-
-    DatabaseController::save($logentries);
+} else {
+    // Prepare for the dashboard output.
+    // Answer the questions.
 }
 
 $mustacheEngine = new Mustache_Engine([
+    'pragmas' => [Mustache_Engine::PRAGMA_BLOCKS],
     'entity_flags' => ENT_QUOTES,
     'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/views'),
 ]);
 
 echo $mustacheEngine->render('mainframe', [
     'appname' => $appname,
+    'version' => $version,
     'bootstrap_css' => $bootstrap_css,
     'bootstrap_js' => $bootstrap_js,
+    'uploadresult' => $uploadresult,
 ]);
